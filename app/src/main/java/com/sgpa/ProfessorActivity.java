16da02e6e9f475dac7393aff56a04eb046b068ac
Gabriel Rodrigues <rodriguez.gabriel001@gmai.com>
2017-07-14 18:23:34 -0300

    Layout professor criado, mas nenhum trigger adicionado. Incluso usuarios.

--- app/src/main/java/com/sgpa/ProfessorActivity.java
+++ app/src/main/java/com/sgpa/ProfessorActivity.java
@@ -3,11 +3,11 @@ package com.sgpa;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MomentosAcitivity extends AppCompatActivity {
+public class ProfessorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_momentos);
+        setContentView(R.layout.activity_professor);
     }
 }
