e14a8c38bf4b275ba4527a3c6c88353f2c4c9273
Gabriel Rodrigues <rodriguez.gabriel001@gmai.com>
2017-07-14 18:39:07 -0300

    Layout disciplina criado, mas nenhum trigger adicionado.

---
+++ app/src/main/java/com/sgpa/DisciplinaActivity.java
@@ -0,0 +1,13 @@
+package com.sgpa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DisciplinaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disciplina);
+    }
+}
