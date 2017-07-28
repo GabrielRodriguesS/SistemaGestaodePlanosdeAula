3ac2bbc27ab41fbd6da00b8d1523851fcd64340f
Luís Rogério Ventura Martins Filho <luisrogeriojf@hotmail.com>
2017-07-28 17:18:39 -0300

    Recursos - Criar

---
+++ app/src/main/java/com/sgpa/activitys/RecursosActivity.java
@@ -0,0 +1,15 @@
+package com.sgpa.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sgpa.R;
+
+public class RecursosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_recursos);
+    }
+}
