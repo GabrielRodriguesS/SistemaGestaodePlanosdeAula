7a69aec3f5ecb032bb715921a00decefc353ed3b
Luís Rogério Ventura Martins Filho <luisrogeriojf@hotmail.com>
2017-07-31 13:56:21 -0300

    Enfileiramento das threads de POST

--- app/src/main/java/com/sgpa/utils/WebClient.java
+++ app/src/main/java/com/sgpa/utils/WebClient.java
@@ -16,7 +16,7 @@ import okhttp3.Response;
 
 public class WebClient implements Runnable {
 
-    private static final String IP = "192.168.0.102";
+    private static final String IP = "10.2.3.117";
     private String url;
     private String json;
     private boolean isPostMethod;
