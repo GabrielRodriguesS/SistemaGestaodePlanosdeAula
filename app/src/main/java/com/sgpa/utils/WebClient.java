086a23acb64b0cb840b601842580de8130b84dce
Luís Rogério Ventura Martins Filho <luisrogeriojf@hotmail.com>
2017-07-30 21:43:19 -0300

    Enfileiramento das threads de POST

--- app/src/main/java/com/sgpa/utils/WebClient.java
+++ app/src/main/java/com/sgpa/utils/WebClient.java
@@ -6,6 +6,8 @@ import com.google.gson.JsonParser;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -54,10 +56,19 @@ public class WebClient implements Runnable {
                 .post(requestBody)
                 .addHeader("content-type", "application/json; charset=utf-8")
                 .build();
-        Response response = client.newCall(request).execute();
-        if (response.isSuccessful()) {
-            this.setJson(response.body().string());
-        }
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    WebClient.this.setJson(response.body().string());
+                }
+            }
+        });
     }
 
     @Override
