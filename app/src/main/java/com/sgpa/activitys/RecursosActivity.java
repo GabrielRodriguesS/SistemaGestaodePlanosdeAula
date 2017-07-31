155f528a07926cbd4f521968c6613494cdcccf63
Luís Rogério Ventura Martins Filho <luisrogeriojf@hotmail.com>
2017-07-31 16:05:42 -0300

    Salvar recurso do momento

--- app/src/main/java/com/sgpa/activitys/RecursosActivity.java
+++ app/src/main/java/com/sgpa/activitys/RecursosActivity.java
@@ -2,8 +2,8 @@ package com.sgpa.activitys;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.sgpa.R;
@@ -13,17 +13,28 @@ import com.sgpa.utils.ViewUtils;
 public class RecursosActivity extends AppCompatActivity {
 
     private Recursos recurso;
+    private long momentoId;
+    private boolean isAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recursos);
+        if (getIntent().hasExtra("isAdd")) {
+            this.isAdd = getIntent().getBooleanExtra("isAdd", false);
+            if (getIntent().hasExtra("momentoId")) {
+                this.momentoId = getIntent().getLongExtra("momentoId", 0);
+            }
+        }
         this.recurso = new Recursos();
     }
 
-    public void getAttributesFromView(View view){
+    public void getAttributesFromView(View view) {
         View rootView = getWindow().getDecorView().getRootView();
         this.recurso.setLink(ViewUtils.getValue(rootView, R.id.link));
+        if (isAdd) {
+            this.recurso.save();
+        }
         Intent returnIntent = new Intent();
         returnIntent.putExtra("recurso", this.recurso);
         setResult(Activity.RESULT_OK, returnIntent);
